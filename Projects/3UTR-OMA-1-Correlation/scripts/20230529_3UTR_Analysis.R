d19a55ed3b2aae324220429eba7d59d4097270b7
Yash Shukla <yash0311@utexas.edu>
2023-05-11 16:43:49 -0500

    completed script, got the p-value

--- Projects/3UTR-OMA-1-Correlation/scripts/20230529_3UTR_Analysis.R
+++ Projects/3UTR-OMA-1-Correlation/scripts/20230529_3UTR_Analysis.R
@@ -3,7 +3,7 @@
 library(ribor)
 library(tidyverse)
 library(readxl)
-library("biomaRt")
+
 
 
 # import the ribo file 
@@ -20,16 +20,34 @@ rc_UTR_1cell_A <- get_region_counts(ribo.object    = original.ribo,
                             region     = "UTR3",
                             compact    = FALSE,
                             experiment = "1cell_A" )
-rc_UTR_1cell_A_top_100 <- rc_UTR_1cell_A |>  
+rc_UTR_1cell_A_top_100 <- rc_UTR_1cell_A |> 
+  arrange(desc(count)) |>  
+  filter (count > 119)
+# cleaning the data to extract the wbgeneIDs for each: 
+
+wbgene_rc_UTR_1cell_A_top_100 <- rc_UTR_1cell_A_top_100 |> 
+  separate(transcript, sep = "gene:",  into = c('a','b'), extra = "merge") |> 
+  separate(b,sep = ".1\\|gene_biotype:", extra = "merge", into = c ('Wbgene_id','d')) |> 
+  select(Wbgene_id)
   
 
-g# import the excel file of oma-1 pull down 
-oma_1_pull_down <- read_excel("data/FPKM_OMA-1_Pull_down.xlsx")
+# import the excel file of oma-1 pull down 
+oma_1_pull_down <- read_excel("data/FPKM_OMA-1_Pull_down_wbgene.xlsx")
 
 # Let us first clean up the oma_1_pull_down data 
 oma_1_pull_down_top_200 <- oma_1_pull_down |>  
-  select(tracking_id,gene_id,FPKM) |> 
-  filter(FPKM > 480)
+  filter(FPKM > 480) |>  
+  rename(Wbgene_id = converted_alias ) |>  
+  select(Wbgene_id)
 
+# finding number of overlap
+overlapping_genes <- wbgene_rc_UTR_1cell_A_top_100 |> 
+  semi_join(oma_1_pull_down_top_200 )
+number_overlapping <- nrow(overlapping_genes)
 
-
+fmat = matrix (nrow =2, ncol = 2)
+fmat[1,] = c(number_overlapping, nrow(wbgene_rc_UTR_1cell_A_top_100 ) - number_overlapping)
+fmat[2,] = c(nrow(oma_1_pull_down_top_200) - number_overlapping, 24244- nrow(oma_1_pull_down_top_200) - fmat[1,2])
+statistical_test_results = fisher.test(fmat)
+statistical_test_results
+print(statistical_test_results$p.value)
